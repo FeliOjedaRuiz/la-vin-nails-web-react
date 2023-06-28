1f602a5639e765b3b4691c752c74b98a7f94c3cb
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2023-06-28 18:23:17 +0200

    Service description see/hide

--- web/src/components/services/service-item/ServiceItem.jsx
+++ web/src/components/services/service-item/ServiceItem.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function ServiceItem({ service }) {
-  
+  const [view, setView] = useState()
+  const [box, setBox] = useState()
+  const [bigBox, setBigBox] = useState()
+  const [icon, setIcon] = useState()
+  const visible = "visible"
+  const hidden = "hidden"
+  const active = "border"
+  const inactive = ""
+  const up = "M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7"
+  const down = "m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+
+  useEffect(() => {
+    setView(hidden)
+    setBox(active)
+    setBigBox(inactive)
+    setIcon(down)
+  },[])
+
+  const handleClick = () => {
+    setView(view === hidden ? visible : hidden)
+    setBox(box === active ? inactive : active)
+    setBigBox(bigBox === active ? inactive : active)
+    setIcon(icon === down ? up : down)
+  }
 
   return (
     <>      
-      <div className=" mx-1 mb-4 p-3 bg-gradient-to-b from-white to-gray-200 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+      <div className=" mx-1 mb-4 p-3 bg-green-50 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl">
           <div className='flex grid-flow-row mb-3'>
             <img className="object-cover rounded h-36 w-36 md:h-48 md:w-48" src={service.image} alt={service.name}/>
             <div className='flex grow justify-center'>
@@ -14,26 +37,20 @@ function ServiceItem({ service }) {
                   <h6 className=" text-xl text-center font-bold tracking-tight text-green-600">desde €{service.price}</h6>
                   <button type="button" class="text-white bg-green-700  hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-200 font-medium w-32 rounded-lg text-md px-3 py-1.5 text-center mb-2">Solicitar cita</button>
               </div>
+            </div>            
+          </div>
+          <div className={`${bigBox} border-gray-300 rounded`}>
+            <div className={` ${box} flex justify-between items-center px-3 py-1 border-gray-300 rounded`}
+              onClick={handleClick} >
+              <h6 className=" text-base font-bold tracking-tight text-gray-900 dark:text-white">Descripción:</h6>
+              <svg class="w-4 h-4 text-pink-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d={`${icon}`}/>
+              </svg>          
             </div>
             
+            <p className={` ${view} text-xs font-normal whitespace-normal m-2 text-gray-700 dark:text-gray-400`}>{service.description}</p>
           </div>
-          <div>
-          <h6 className=" text-base font-bold tracking-tight text-gray-900 dark:text-white">Descripción:</h6>  
-          <p className=" text-xs font-normal text-gray-700 dark:text-gray-400">{service.description}</p>
-            
-            
-
-
-          </div>
-      </div>
-
-
-      
-        
-
-
-      
-     
+      </div>   
     </>
   )
 }
