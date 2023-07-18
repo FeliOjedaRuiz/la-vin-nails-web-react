fcc0c724a925004c39e4ecdbb7fe4343a2b2dc8b
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2023-07-18 20:06:42 +0200

    set the dates of the days of the week

--- web/src/components/turns/turns-list-by-week/TurnsListByWeek.jsx
+++ web/src/components/turns/turns-list-by-week/TurnsListByWeek.jsx
@@ -29,14 +29,46 @@ function TurnsListByWeek() {
   useEffect(() => {
     turnsService.list()
       .then((turns) => {
-        console.log(`filtro Ini: ${initDate} Fin: ${finalDate}`)
         const weekTurns = turns.filter(turn => turn.date >= initDate && turn.date <= finalDate)
         setTurns(weekTurns)        
       })
       .catch(error => console.error(error));
   }, [initDate]); 
 
-  console.log(turns)
+  // console.log(turns)
+  // console.log(`filtro Ini: ${initDate} Fin: ${finalDate}`)
+
+  // const dayStr = new Date(initDate)
+  // const utcDay = Date.parse(initDate)
+
+  const dia = new Date(Date.parse(initDate))
+ 
+
+  const transformDate = (date) => {    
+    let dt = new Date(date);
+    const year = dt.getFullYear()
+    let month = dt.getMonth() +1
+    let day = dt.getDate()
+
+    if (month < 10) {
+      month = "0"+month
+    }
+
+    if (day < 10){
+      day = "0"+day
+    }
+
+    return `${year}-${month}-${day}`;
+  };
+
+  const secondDay = transformDate(dia.setDate(dia.getDate() + 1))
+  const thirdDay = transformDate(dia.setDate(dia.getDate() + 2))
+  const fourthDay = transformDate(dia.setDate(dia.getDate() + 3))
+  const fifthDay = transformDate(dia.setDate(dia.getDate() + 4))
+  const sixthDay = transformDate(dia.setDate(dia.getDate() + 5))
+
+  console.log(`Semana: ${initDate} - ${secondDay} - ${thirdDay}`)
+  
   
 
   return (
