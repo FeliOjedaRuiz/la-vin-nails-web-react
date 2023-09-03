6c81dd2a01ed49b6d5733a2bb9dd7a3285a4503a
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2023-09-03 23:35:38 +0200

    view of available turns without login

--- web/src/pages/SchedulePageGuest.jsx
+++ web/src/pages/SchedulePageGuest.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/layouts/Layout";
 import datesService from "../services/dates";
-import DateDetail from "../components/dates/date-detail/DateDetail";
 import { HonestWeekPicker } from "../components/week-picker/week-picker-js/HonestWeekPicker";
 import TurnListByWeek from "../components/turns/turn-list-by-week/TurnListByWeek";
 import { NavLink } from "react-router-dom";
 
 function SchedulePageGuest() {
-  const [dates, setDates] = useState([]);
-  const [reload, setReload] = useState(false);
   const [initDate, setInitDate] = useState();
   const [selectedTurn, setSelectedTurn] = useState({});
   const [selectedDate, setSelectedDate] = useState({});
@@ -21,36 +18,8 @@ function SchedulePageGuest() {
     setSelectedTurn(turn);
   };
 
-  const transformDate = (date) => {
-    let dt = new Date(date);
-    let year = dt.getFullYear();
-    let month = dt.getMonth() + 1;
-    let day = dt.getDate();
 
-    if (month < 10) {
-      month = "0" + month;
-    }
 
-    if (day < 10) {
-      day = "0" + day;
-    }
-
-    return `${year}-${month}-${day}`;
-  };
-
-  const actualDate = transformDate(new Date());
-
-  useEffect(() => {
-    datesService
-      .myList()
-      .then((dates) => {
-        const datesUserAndDate = dates.filter(
-          (date) => date.turn.date >= actualDate
-        );
-        setDates(datesUserAndDate);
-      })
-      .catch((error) => console.error(error));
-  }, [reload]);
 
   return (
     <>
@@ -72,8 +41,8 @@ function SchedulePageGuest() {
           </div>
 
           <NavLink to="/services">
-            <div className="text-center mt-4 bg-emerald-600 rounded-lg px-3 py-2 leading-tight text-white text-lg">
-              Para seleccionar un turno disponible debes acceder a la sección de
+            <div className="text-center mt-4 mx-2 bg-emerald-600 rounded-lg px-3 py-2 leading-tight text-white text-lg">
+              Para solicitar cita debes acceder a la sección de
               servicios.
               <p className="font-bold">Click Aquí</p>
             </div>            
