96f861f3793fe9a8f1a42799db2967a3435b3b6a
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2024-02-20 17:01:44 +0100

    Styling error page

--- web/src/pages/ErrorPage.jsx
+++ web/src/pages/ErrorPage.jsx
@@ -6,10 +6,17 @@ function ErrorPage() {
   return (
     <>
       <Layout>
-        <div className="flex flex-col justify-center p-4">
-          <div className="text-center text-3xl">Error Page</div>
-          <div>
-            <img alt="Oops! Error" src={errorImage}></img>
+        <div className="flex flex-col h-full justify-center items-center p-4">
+          <p className="text-5xl -mb-16 font-bold text-pink-600">¡Opps!</p>
+          <img className="max-w-xs" alt="Oops! Error" src={errorImage}></img>
+          <p className="text-lg/5 text-center font-medium text-emerald-700">
+            Ah ocurrido un error inesperado.
+          </p>
+          <div className="max-w-xs text-center mt-10">
+            <p className="text-sm/4 font-medium text-pink-700">
+              Disculpa el inconveniente, si el error persite, comunicate con el
+              administrador.{" "}
+            </p>
           </div>
         </div>
       </Layout>
