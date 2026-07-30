76b56b17d470ad8c03198a45a01c5cee297189c4
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2026-07-30 23:15:50 +0200

    feat(turns): filter list by category query param

--- web/src/services/turns.js
+++ web/src/services/turns.js
@@ -2,8 +2,37 @@ import http from "./base-api";
 
 const create = (turn) => http.post("/turns", turn);
 
-const list = (date, endDate, signal) => {
-  const url = endDate ? `/turns/date/${date}?endDate=${endDate}` : `/turns/date/${date}`;
+const list = (date, endDate, params, maybeSignal) => {
+  // Supports legacy calls: list(date, endDate, signal)
+  // Supports category-aware calls: list(date, endDate, category, signal)
+  // Supports params object: list(date, endDate, { category, signal })
+  let category;
+  let signal;
+
+  if (typeof params === 'string') {
+    category = params;
+    signal = maybeSignal;
+  } else if (params && typeof params === 'object') {
+    if (
+      typeof params.abort === 'function' &&
+      'onabort' in params &&
+      params.category === undefined
+    ) {
+      signal = params;
+    } else {
+      category = params.category;
+      signal = params.signal;
+    }
+  }
+
+  const query = new URLSearchParams();
+  if (endDate) query.set('endDate', endDate);
+  if (category) query.set('category', category);
+  const queryString = query.toString();
+  const url = queryString
+    ? `/turns/date/${date}?${queryString}`
+    : `/turns/date/${date}`;
+
   return http.get(url, signal ? { signal } : {});
 };
 
